fd61c7fa26d69c32a3b26f80d91e54a20093b67b
LeeKennedy <mail@leekennedy.id.au>
2017-09-17 18:36:20 +1000

    Relabelled Columns

--- Scripts/basic_summary.R
+++ Scripts/basic_summary.R
@@ -31,6 +31,15 @@ data.in3 <- as.data.frame(data.in3)
 data.in4 <- spread(data.in3, STATUS, Freq)
 data.in4 <- data.in4[c(2,1),]
 
+for(i in 2:6){
+if(colnames(data.in4)[i] == "A") colnames(data.in4)[i] = "Authorised"
+if(colnames(data.in4)[i] == "N") colnames(data.in4)[i] = "Not Entered"
+if(colnames(data.in4)[i] == "E") colnames(data.in4)[i] = "Entered"
+if(colnames(data.in4)[i] == "R") colnames(data.in4)[i] = "Rejected"
+if(colnames(data.in4)[i] == "M") colnames(data.in4)[i] = "Modified"
+if(colnames(data.in4)[i] == "X") colnames(data.in4)[i] = "Cancelled"
+}
+
 write.csv(data.in4, "Proteins_Aug_17.csv")
 
 #### Visualising Data -------------------------------------------------------
